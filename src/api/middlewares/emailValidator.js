c7d246245294febccd7dbfbf317b77306b354802
Marcilane Souza <marcilane29@gmail.com>
2022-07-27 15:56:29 -0300

    add emailValidator

---
+++ src/api/middlewares/emailValidator.js
@@ -0,0 +1,17 @@
+import { getUsers } from '../models/UserModel.js';
+
+function emailValidator(req, res, next){
+    const { email } = req.body;
+    const data = getUsers();
+    const user = data.find((item)=>{
+        return item.email === email
+    })
+    if(!user){
+        return res.status(400).json({
+            status: 'Email já cadastrado!'
+        })
+    }
+    next();
+}
+
+export default emailValidator
