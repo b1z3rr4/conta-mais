ff69a878afc30131834ea5bfa75e291780ae7b0b
Cleo Silva <cleo_silva1906@hotmail.com>
2022-09-06 21:17:18 -0300

    Criado métodos service

--- api/services/bankAccount/createBankAccount.service.js
+++ api/services/bankAccount/createBankAccount.service.js
@@ -1,31 +1,40 @@
-import BusinessRepository from "../../repositories/businessRepository";
+import bankAccountRepository from "../../repositories/bankAccountRepository";
 import generatorId from "../../../utils/generatorId";
 
-class createBusinessService {
+class createBankAccountService {
   constructor() {}
 
-  async createBusiness(cnpj, company, bank_stock) {
+  async createBankAccount(
+    id,
+    angency,
+    account,
+    financial_institution,
+    cash,
+    limit
+  ) {
     const id = generatorId();
-    this.repository = new BusinessRepository();
+    this.repository = new bankAccountRepository();
     try {
-      this.business = await this.repository.create(
+      this.bank = await this.repository.create(
         id,
-        cnpj,
-        company,
-        bank_stock
+        angency,
+        account,
+        financial_institution,
+        cash,
+        limit
       );
       return {
         status: 201,
-        message: this.business,
+        message: this.bank,
       };
     } catch (e) {
       console.log(e);
       return {
         status: 400,
-        message: "Não foi possível cadastrar a nova empresa.",
+        message: "Não foi possível cadastrar a nova conta.",
       };
     }
   }
 }
 
-export default createBusinessService;
+export default createBankAccountService;
--- api/services/business/updateBusiness.service.js
+++ api/services/business/updateBusiness.service.js
@@ -1,35 +1,35 @@
 import BusinessRepository from "../../repositories/businessRepository";
 
 class updateBusinessService {
-    constructor(){}
+  constructor() {}
 
-    async updateBusiness(id, cnpj, company, bank_stock){
-        this.repository = new BusinessRepository();
-        const obj = {
-            cnpj,
-            company,
-            bank_stock
-        }
-        try{
-            this.business = await this.repository.update(id, obj);
-            if(this.business[0] === 1){
-            return {
-                status: 200,
-                message: 'Usuário atualizado!'
-            }
-        }
-           return {
-                status: 400,
-                message: "Não foi possível atualizar a empresa."
-           }
-            } catch(e){
-                console.log(e);
-                return {
-                    status: 400,
-                    message: "Não foi possível atualizar a empresa."
-                }
-             }
-        }
-   }
+  async updateBusiness(id, cnpj, company, bank_stock) {
+    this.repository = new BusinessRepository();
+    const obj = {
+      cnpj,
+      company,
+      bank_stock,
+    };
+    try {
+      this.business = await this.repository.update(id, obj);
+      if (this.business[0] === 1) {
+        return {
+          status: 200,
+          message: "Empresa atualizada!",
+        };
+      }
+      return {
+        status: 400,
+        message: "Não foi possível atualizar a empresa.",
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        status: 400,
+        message: "Não foi possível atualizar a empresa.",
+      };
+    }
+  }
+}
 
 export default updateBusinessService;
