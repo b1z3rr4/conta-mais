7597946b0a1ab8824afa9cf4ecf761df636a466c
Cleo Silva <cleo_silva1906@hotmail.com>
2022-09-20 20:15:15 -0300

    Criado documentação do Business

--- routes/business/businessRoutes.js
+++ routes/business/businessRoutes.js
@@ -5,9 +5,9 @@ import businessValidator from "../../api/middlewares/businessValidator.js";
 
 const routesBusiness = Router();
 
-routesBusiness.get('/business', authValidate, businessValidator.getValidator, BusinessController.listBusiness);
+routesBusiness.get('/business/list', authValidate, businessValidator.getValidator, BusinessController.listBusiness);
 routesBusiness.post('/business', authValidate, businessValidator.postValidator, BusinessController.createBusiness);
-routesBusiness.put('/business/:id', authValidate, businessValidator.putValidator, BusinessController.updateBusiness);
-routesBusiness.delete('/business/:id', authValidate, businessValidator.deleteValidator, BusinessController.deleteBusiness);
+routesBusiness.put('/updateBusiness/:id', authValidate, businessValidator.putValidator, BusinessController.updateBusiness);
+routesBusiness.delete('/deleteBusiness/:id', authValidate, businessValidator.deleteValidator, BusinessController.deleteBusiness);
 
 export default routesBusiness;
